6138518dc3390982163daacf760e891f161f96f0
jiteshjitsun <jiteshjitsun@gmail.com>
2025-01-04 04:46:25 +0530

    this one is predecided

--- Patterns/pattern.cpp
+++ Patterns/pattern.cpp
@@ -190,6 +190,65 @@ void pattern12(int row) {
     }
 }
 
+void pattern13(int row) {
+    for(int i=1; i<=row/2; i++) {
+        //stars
+        for(int j=0; j<i; j++){
+            cout<<"*";
+        }
+        //space
+        for(int j=0; j<row-(2*i); j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=0; j<i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=row/2-1; i>0; i--) {
+        //stars
+        for(int j=0; j<i; j++){
+            cout<<"*";
+        }
+        //space
+        for(int j=0; j<row-(i*2); j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=0; j<i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern14(int row) {
+    for(int i=0; i<row; i++) {
+        for(int j=0; j<row; j++) {
+            if(i==0 || i==row-1 || j==0 || j==row-1) {
+                cout<<"*";
+            }else {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+void pattern15(int row) {
+    for(int i=0; i<2*row-1; i++) {
+        for(int j=0; j<2*row-1; j++) {
+            int top = i;
+            int left = j;
+            int right = (2*row - 2) - j;
+            int bottom = (2*row - 2) - i;
+            cout<<(row - min(min(top, bottom), min(left, right)));
+        }
+        cout<<endl;
+    }
+}
+
 int main() {
     int row;
     cin>>row;
@@ -204,5 +263,8 @@ int main() {
     // pattern9(row);
     // pattern10(row);
     // pattern11(row);
-    pattern12(row);
+    // pattern12(row);
+    // pattern13(row);
+    // pattern14(row);
+    pattern15(row);
 }
