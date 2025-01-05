aaa9bbc2e7c1a5c7c8edec8719015ff40592c7e3
jiteshjitsun <jiteshjitsun@gmail.com>
2025-01-05 13:06:23 +0530

    stl

---
+++ Patterns/pairs.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// pairs is a part of utility library
+void explainPair() {
+    pair<int, int> p = {1, 3};
+    cout<<p.first<<" "<<p.second;
+}
+
+int main() {
+    explainPair();
+}
